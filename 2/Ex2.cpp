c88e83fd30c73c68068d74e2c3b1e2e0e06e4ccf
ShacharTs <shachar.ts1@gmail.com>
2025-04-03 19:58:15 +0300

    Ex2

--- 2/Ex2.cpp
+++ 2/Ex2.cpp
@@ -1,11 +1,50 @@
-#include <complex.h>
+#include <complex>
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+void mandelbrot(double real, double imag) {
+        int limits = 10000;
+        double zReal = 0.0;
+        double zImag = 0.0;
+
+
+        for (int i = 0; i < limits; i++) {
+                const double r2 = zReal * zReal;
+                const double i2 = zImag * zImag;
+
+                // Escape condition: if |z|^2 > 4, then the point escapes to infinity
+                if (r2 + i2 > 4.0) {
+                        cout << real << " + " << imag << "i" << " is not in mandelbrot set" << endl;
+                        return;
+                }
+
+                // Update z = z^2 + c
+                zImag = 2.0 * zReal * zImag + imag;
+                zReal = r2 - i2 + real;
+        }
+
+        // If it doesn't escape, it's in the Mandelbrot set
+        cout << real << " + " << imag << "i" << " is in mandelbrot set" << endl;
+}
+
+int main(int argc, char* argv[]){
+
+        if (argc != 3) {
+                cout << "Usage: " << argv[0] << " <real> <imaginary>" << endl;
+                return 1;
+        }
+
+        const double real = stod(argv[1]);
+        const double imag = stod(argv[2]);
+        mandelbrot(real, imag);
 
 
 
-int main(){
 
-    _complex<double> z0 (3.1,4.5);
+
 
 
 
